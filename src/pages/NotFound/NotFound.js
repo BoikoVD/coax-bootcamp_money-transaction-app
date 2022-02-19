92652ab2dcf6147cc11b30a997b446bb2a75bfa4
BoikoVD <boiko.volodymyr.dm@gmail.com>
2022-02-20 00:09:40 +0200

    Add styles for NotFound page

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import classes from './NotFound.module.scss';
 
 function NotFound() {
 
-	return (
-		<div>
-			NotFound Page
-		</div>
-	)
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.title}>
+        404
+      </div>
+      <div className={classes.text}>
+        Page not found
+      </div>
+    </div>
+  )
 }
 
 export default NotFound;
